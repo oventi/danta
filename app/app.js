b01ef61518a019e6cc40125b7dcae0222f60a8a4
Andrés Proaño <andres@oventi.org>
2014-03-26 00:23:11 -0300

    removed comment from base app

--- app/app.js
+++ app/app.js
@@ -2,8 +2,6 @@ danta.app(function () {
     var app = this;
     var w = null;
     
-    //danta.config.REMOTE_BASE_PATH = "/danta";
-    
     this.init = function () {
         w = this.widgets;
     }
