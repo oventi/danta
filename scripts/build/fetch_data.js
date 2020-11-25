aa2eef9d3e4d47c3a92a935c6f604ec8c5e2b5f7
Andrés Proaño <andres@actionstation.org.nz>
2020-11-25 18:36:43 +1300

    fetch contentful data and build nav, make it into a lib

--- scripts/build/fetch_data.js
+++ scripts/build/fetch_data.js
@@ -1,26 +1,11 @@
-import axios from 'axios'
-
-// IMPORTANT, strapi is installed on /usr/local/strapi
+import {get_data} from '../../lib/contentful'
 
 export async function fetch_data() {
-  const {STRAPI_ENDPOINT} = process.env
-
   /* START CUSTOM */
 
-  const get = async path => {
-    const response = await axios.get(`${STRAPI_ENDPOINT}${path}`)
-
-    return response.data
-  }
-
-  const {title, short_description} = await get('/common')
-
-  console.log('TITLE', title)
-  console.log('SHORT', short_description)
-
-  return {
-    title, short_description
-  }
+  // IMPORT: MAKE SURE THERE IS NO MORE THAN 10 levels of linking
+  // https://www.contentful.com/developers/docs/concepts/links/
+  return await get_data({content_type: 'site', 'fields.name': 'site-test'})
 
   /* END CUSTOM */
 }
--- scripts/dev2.js
+++ scripts/dev2.js
@@ -8,35 +8,32 @@ import {execSync as exec} from 'child_process'
 import path from 'path'
 import {set_env, fetch_data, build_html} from './build'
 
+const data_filepath = path.resolve(__dirname, '../data_dev.json')
+let data = {}
+
 set_env('dev')
 
-const get_nav = ({pages, extra_nav_links}) => {
-  const nav_links = pages.map(({name, title}) => (
-    {url: `/${name}.html`, label: title}
-  ))
-
-  return extra_nav_links ? [...nav_links, ...extra_nav_links] : nav_links
-}
-
 async function build_dev() {
-  const data = await fetch_data('dev')
-  const nav = get_nav(data)
+  if(!file_exists(data_filepath)) {
+    data = await fetch_data('dev')
+    //file_write('./data_dev.json', JSON.stringify(data))
+  }
 
-  console.log(data)
+  //data = JSON.parse(file_read('./data_dev.json', 'utf8'))
 
-  // building navigation
-  console.log(nav)
-
-  /*
   const html = await build_html(data)
 
   // TODO, support constants in webpack.config.js
-  //exec('webpack')
+  exec('webpack')
 
   // TODO run scss
   exec('yarn sass ./client/scss/index.scss dist/index.css')
 
   const injected_html = html
+    .replace('</body>', `
+        <script src="/index.js"></script>
+      </body>
+    `)
     .replace('</head>', `
         <link rel="stylesheet" href="/index.css">
       </head>
@@ -44,17 +41,12 @@ async function build_dev() {
 
   // TODO: minify html
   file_write('./dist/index.html', injected_html)
-  */
 }
 
-/*
 const StaticServer = require('static-server')
 const server = new StaticServer({rootPath: './dist', port: 3000, cors: '*', followSymlink: true})
 
 server.on('request',  async (req, res) => await build_dev())
 server.start(() => console.log('danta app running on', server.port))
-*/
 
 //setInterval(async () => await build_dev(), 1000)
-
-(async () => await build_dev())()
